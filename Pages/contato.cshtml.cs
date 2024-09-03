5eade5f77817b103b57eba1f31e1ff4be7fde7d6
alexandre dias <xanderio@outlook.com>
2024-09-02 21:36:17 -0300

    Pagina de contato

---
+++ Pages/contato.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace escola.Pages
+{
+    public class contatoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
