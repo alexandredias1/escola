7c8929eb768748ca713718253911296bb88c9d1d
alexandre dias <xanderio@outlook.com>
2024-09-04 19:53:00 -0300

    pagina sobre nos

---
+++ Pages/SobreNos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace escola.Pages
+{
+    public class SobreNosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
